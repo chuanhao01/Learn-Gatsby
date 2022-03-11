36995169efc1c4895fe7a82c10912315293d5036
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2022-03-11 16:11:39 +0800

    [feat] adding an image

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,10 +1,17 @@
+import { StaticImage } from "gatsby-plugin-image";
 import * as React from "react";
 import { SimpleLayout } from "../components/layouts/SimpleLayout";
 
 const About = (): JSX.Element => {
   return (
     <SimpleLayout pageTitle="About Page">
-      <h1>This is the about pagge</h1>
+      <>
+        <h1>This is the about pagge</h1>
+        <StaticImage
+          src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+          alt="some guy"
+        />
+      </>
     </SimpleLayout>
   );
 };
