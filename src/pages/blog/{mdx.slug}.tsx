b68144e858b39583c07cae64654a651cb9feb246
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2022-03-18 21:43:28 +0800

    [WIP] working on blog migration

---
+++ src/pages/blog/{mdx.slug}.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import { SimpleLayout } from "../components/layouts/SimpleLayout";
+
+const IndexPage = (): JSX.Element => {
+  return (
+  );
+};
+
+export default IndexPage;
