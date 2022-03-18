850cfe6466deee1ca597cb4e3d48ac4a87c94acf
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2022-03-18 16:00:50 +0800

    [feat] adding blog query

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,11 +1,37 @@
+import { graphql, useStaticQuery } from "gatsby";
 import * as React from "react";
 import { SimpleLayout } from "../components/layouts/SimpleLayout";
 
 const Blog = (): JSX.Element => {
+  interface IBlogData {
+    allFile: {
+      nodes: {
+        name: string;
+        absolutePath: string;
+        sourceInstanceName: string;
+      }[];
+    };
+  }
+  const blogData: IBlogData = useStaticQuery(graphql`
+    {
+      allFile(filter: { sourceInstanceName: { eq: "blog" }, name: {} }) {
+        nodes {
+          name
+          absolutePath
+          sourceInstanceName
+        }
+      }
+    }
+  `);
   return (
     <SimpleLayout pageTitle="Blog Page">
       <>
         <p>Look at the blog page</p>
+        <ul>
+          {blogData.allFile.nodes.map((node) => (
+            <li key={node.name}>{node.name}</li>
+          ))}
+        </ul>
       </>
     </SimpleLayout>
   );
