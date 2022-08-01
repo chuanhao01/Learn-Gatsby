426307b7a50f0e9b9fc36dfcc3b24fb6c49da14b
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2022-08-01 21:01:23 +0800

    [feat] config to follow name of repo and folder

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,8 +2,8 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `Learning Gatsby`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `Learn Gatsby`,
+    siteUrl: ``,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
