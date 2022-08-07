40ba70dc9c3408b3fa1fa2a705ff240e5e399511
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2022-08-07 16:24:39 +0800

    [feat-fix] prettier sort imports
    Description
    - Needed newer version of prettier
    - Generally, because we need vscode prettier extension to use locall
      installed prettier
    - Local Prettier is able to detect plugins and run fine

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  bracketSpacing: true,
+  importOrder: ["^[./]"],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+};
