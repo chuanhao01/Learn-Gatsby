18c815c04ad57c7cfa4f97e2e975d593e87271f2
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2022-08-13 12:18:23 +0800

    [feat] type gatsby-node

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,7 +1,12 @@
+import { GatsbyNode } from "gatsby";
+
 import path from "path";
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-exports.onCreateWebpackConfig = ({ _, actions }) => {
+export const onCreateWebpackConfig: GatsbyNode["onCreateWebpackConfig"] = ({
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  _,
+  actions,
+}) => {
   actions.setWebpackConfig({
     resolve: {
       modules: [path.resolve(__dirname, "src"), "node_modules"],
