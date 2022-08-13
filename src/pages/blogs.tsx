fd744b5b28b508ae6759e50aa25df68c6a83b876
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2022-08-13 15:25:36 +0800

    [feat] adding blogs page

---
+++ src/pages/blogs.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+import { VerticalPageLayout } from "components/layouts/VerticalPageLayout";
+
+const BlogsPage = () => {
+  return (
+    <VerticalPageLayout pageTitle="My Blog Posts">
+      <p>Look at my blogs</p>
+    </VerticalPageLayout>
+  );
+};
+
+export default BlogsPage;
