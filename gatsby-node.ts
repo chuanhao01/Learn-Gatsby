46ad5360bed272c7395438946747edce9314c02d
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2022-08-13 15:05:59 +0800

    [feat] adding site type defs graphql

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -2,6 +2,22 @@ import { GatsbyNode } from "gatsby";
 
 import path from "path";
 
+export const createSchemaCustomization: GatsbyNode["createSchemaCustomization"] =
+  ({ actions }) => {
+    const { createTypes } = actions;
+
+    const siteTypesDefs = `
+    type Site {
+      siteMetadata: SiteMetadata!
+    }
+    type SiteMetadata {
+      title: String!
+    }
+    `;
+
+    createTypes(siteTypesDefs);
+  };
+
 export const onCreateWebpackConfig: GatsbyNode["onCreateWebpackConfig"] = ({
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   _,
